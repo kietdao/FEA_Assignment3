78ac7fb9189fa55a76592ffc9b7f1f82757e853c
kietdao <43434606+kietdao@users.noreply.github.com>
2022-04-19 15:47:51 +0700

    seperate files callback and promise

---
+++ callback.js
@@ -0,0 +1,26 @@
+const gioBatDauDi = 19
+// CALLBACK
+function con_tho_an_co(gioAn, diXemPhim) {
+    return setTimeout(() => {
+        console.log(`Con thỏ ăn cỏ lúc: ${gioAn}h`)
+        console.log(`Con thỏ ăn xong lúc: ${gioAn + 1}h`)
+        diXemPhim(gioAn + 1, con_tho_di_dao)
+    }, 1000)
+}
+
+function con_tho_di_xem_phim(gioXemPhim, diDao) {
+    return setTimeout(() => {
+        console.log(`Con thỏ đi xem phim xong lúc: ${gioXemPhim + 2}h`)
+        diDao(gioXemPhim + 2)
+    }, 2000)
+}
+
+function con_tho_di_dao(gioDiDao) {
+    return setTimeout(() => {
+        console.log(`Con thỏ đi dạo xong lúc: ${gioDiDao + 1}h`)
+        console.log(`Tổng thời gian đi chơi là ${gioDiDao + 1 - gioBatDauDi} tiếng`)
+    }, 1000)
+}
+
+
+con_tho_an_co(gioBatDauDi, con_tho_di_xem_phim)
