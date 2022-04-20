0169dc0070f86c469b7d218ea7f28a27922d6811
kietdao <43434606+kietdao@users.noreply.github.com>
2022-04-20 19:01:35 +0700

    edit callback.js

--- callback.js
+++ callback.js
@@ -1,17 +1,17 @@
 const gioBatDauDi = 19
 // CALLBACK
-function con_tho_an_co(gioAn, diXemPhim) {
+function con_tho_an_co(gioAn, hanhdongkhac) {
     return setTimeout(() => {
         console.log(`Con thỏ ăn cỏ lúc: ${gioAn}h`)
         console.log(`Con thỏ ăn xong lúc: ${gioAn + 1}h`)
-        diXemPhim(gioAn + 1, con_tho_di_dao)
+        hanhdongkhac(gioAn + 1, con_tho_di_dao)
     }, 1000)
 }
 
-function con_tho_di_xem_phim(gioXemPhim, diDao) {
+function con_tho_di_xem_phim(gioXemPhim, hanhdongkhac) {
     return setTimeout(() => {
         console.log(`Con thỏ đi xem phim xong lúc: ${gioXemPhim + 2}h`)
-        diDao(gioXemPhim + 2)
+        hanhdongkhac(gioXemPhim + 2)
     }, 2000)
 }
 
